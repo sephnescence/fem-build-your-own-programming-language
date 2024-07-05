38beeca39a6fd1a2cdc42276ee723872da01babe
Blake Taylor <sephnescence@gmail.com>
2024-07-05 11:40:17 +1000

    Module 6 complete

--- __tests__/pilot.ts
+++ __tests__/pilot.ts
@@ -1,3 +1,88 @@
-test("adds 1 + 2 to equal 3", () => {
-  expect(1 + 2).toBe(3);
+type Token = {
+  type: string;
+  token: string;
+};
+
+const isParenthesis = ({ token }: { token: string }) => {
+  return token === "(" || token === ")";
+};
+
+const isWhitespace = ({ token }: { token: string }) => {
+  return /\s/.test(token);
+};
+
+const tokenise = ({ input }: { input: string }) => {
+  const tokens: Token[] = [];
+
+  let cursor = 0;
+  while (cursor < input.length) {
+    const token = input[cursor];
+    // console.log(`Encountered token - ${token}`);
+
+    if (isParenthesis({ token })) {
+      // console.log("Is parenthesis");
+
+      tokens.push({
+        type: "Parenthesis",
+        token,
+      });
+      cursor++;
+      continue;
+    }
+
+    if (isWhitespace({ token })) {
+      // console.log("Is whitespace");
+
+      tokens.push({
+        type: "Whitespace",
+        token,
+      });
+      cursor++;
+      continue;
+    }
+
+    // Steve recommended throwing this exception
+    throw new Error(`Invalid token found - ${token}`);
+  }
+
+  return tokens;
+};
+
+describe("Test tokenise response", () => {
+  it("Should return nothing given a blank string", () => {
+    const tokens = tokenise({ input: "" });
+
+    expect(tokens).toEqual([]);
+  });
+
+  it("Should should accept parentheses", () => {
+    const tokens = tokenise({ input: "()" });
+
+    const expected: Token[] = [
+      {
+        type: "Parenthesis",
+        token: "(",
+      },
+      {
+        type: "Parenthesis",
+        token: ")",
+      },
+    ];
+
+    expect(tokens).toEqual(expected);
+  });
+
+  // Steve ignores whitespace in his example, but surely it should be kept, if only to add to an open String
+  it("Should should accept whitespace", () => {
+    const tokens = tokenise({ input: " " });
+
+    const expected: Token[] = [
+      {
+        type: "Whitespace",
+        token: " ",
+      },
+    ];
+
+    expect(tokens).toEqual(expected);
+  });
 });
